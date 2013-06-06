b811dc252f76d9f3e8c2687771600d7494ad1567
Hugh Daschbach <hugh.daschbach@enmotus.com>
2013-06-05 17:02:24 -0700

    Guile module wrapped around Per Bohtner's srfi-64.

---
+++ bohtner/srfi-64.scm
@@ -0,0 +1,20 @@
+(define-module (bohtner srfi-64)
+  #:export-syntax (test-assert
+		   test-eqv
+		   test-equal
+		   test-eq
+		   test-approximate
+		   test-error
+		   test-read-eval-string
+		   test-begin
+		   test-end
+		   test-group
+		   test-group-with-cleanup
+		   test-runner?
+		   test-runner-current
+		   test-runner-get
+		   test-runner-simple
+		   test-runner-null
+		   test-runner-create
+		   test-runner-factory))
+(include "testing.scm")
