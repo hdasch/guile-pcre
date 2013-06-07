c4a47ec6b4d2632f4dbbd5ee21ecee64ac899554
Hugh Daschbach <hugh.daschbach@enmotus.com>
2013-06-07 16:49:00 -0700

    Move primitive matching code into unit tests.
    Print configuration summary before running unit tests.

--- test.scm
+++ test.scm
@@ -12,22 +12,53 @@
 	     (bohtner srfi-64))
 
 (format #t "pcre Version: ~a\n" (pcre-version))
-(format #t "~a\n" (pcre-exec (make-pcre "([[:digit:].]+)abc") "123.456abc789"))
-(format #t "~a\n" (pcre-exec (make-pcre "abc") "123abc456"))
-
-(format #t "~a\n" (pcre-exec (make-pcre "abc" PCRE_CASELESS) "123abc456"))
-(format #t "~a\n" (pcre-exec (make-pcre "abc" PCRE_CASELESS PCRE_STUDY_JIT_COMPILE) "123abc456"))
-(format #t "~a\n" (pcre-exec (make-pcre "abc" PCRE_STUDY_JIT_COMPILE) "123abc456"))
-(let ((re (make-pcre "abc" PCRE_CASELESS)))
-  (format #t "PCRE_INFO_BACKREFMAX: ~a\n" (pcre-fullinfo re PCRE_INFO_BACKREFMAX))
-  (format #t "options: ~a, caseless: ~a\n" (pcre-fullinfo re PCRE_INFO_OPTIONS)
-	  PCRE_CASELESS))
+(format #t "libpcre configuration:\n")
+(format #t "  PCRE_CONFIG_UTF8: ~a\n" (pcre-config PCRE_CONFIG_UTF8))
+(format #t "  PCRE_CONFIG_UTF16: ~a\n" (pcre-config PCRE_CONFIG_UTF16))
+(format #t "  PCRE_CONFIG_UTF32: ~a\n" (pcre-config PCRE_CONFIG_UTF32))
+(format #t "  PCRE_CONFIG_UNICODE_PROPERTIES: ~a\n"
+	(pcre-config PCRE_CONFIG_UNICODE_PROPERTIES))
+(format #t "  PCRE_CONFIG_JIT: ~a\n" (pcre-config PCRE_CONFIG_JIT))
+(format #t "  PCRE_CONFIG_BSR: ~a\n" (pcre-config PCRE_CONFIG_BSR))
+(format #t "  PCRE_CONFIG_NEWLINE: ~a\n" (pcre-config PCRE_CONFIG_NEWLINE))
+(format #t "  PCRE_CONFIG_LINK_SIZE: ~a\n" (pcre-config PCRE_CONFIG_LINK_SIZE))
+(format #t "  PCRE_CONFIG_POSIX_MALLOC_THRESHOLD: ~a\n"
+	(pcre-config PCRE_CONFIG_POSIX_MALLOC_THRESHOLD))
+(format #t "  PCRE_CONFIG_STACKRECURSE: ~a\n"
+	(pcre-config PCRE_CONFIG_STACKRECURSE))
+(format #t "  PCRE_CONFIG_MATCH_LIMIT: ~a\n"
+	(pcre-config PCRE_CONFIG_MATCH_LIMIT))
+(format #t "  PCRE_CONFIG_MATCH_LIMIT_RECURSION: ~a\n"
+	(pcre-config PCRE_CONFIG_MATCH_LIMIT_RECURSION))
+(format #t "  PCRE_CONFIG_JITTARGET: ~a\n" (pcre-config PCRE_CONFIG_JITTARGET))
 
 
 (test-begin "pcre-unit-test")
 (test-assert "pcre?" (pcre? (make-pcre "([[:digit:].]+)abc")))
 (test-assert "pcre-exec "
-	     (pcre-exec (make-pcre "abc" PCRE_CASELESS) "123abc456"))
+	     (pcre-exec (make-pcre "abc" PCRE_CASELESS) "123ABC456"))
+
+(test-assert "caseless match "
+	     (match
+	       (pcre-exec (make-pcre "abc" PCRE_CASELESS) "123ABC456")
+	       (#(string (3 . 6)) #t)
+	       (_ #f)))
+(test-assert "grouping match" 
+	     (match
+	       (pcre-exec (make-pcre
+			   "(\\w+)\\s+(\\w+)\\s+(\\w+)\\s+(\\w+)\\s+(\\w+)")
+			  "The quick brown fox jumped")
+	       (#(string (0 . 26) (0 . 3) (4 . 9) (10 . 15) (16 . 19) (20 . 26)) #t)
+	       (_ #f)))
+
+(test-assert "jit compiled" 
+	     (match
+	       (pcre-exec (make-pcre
+			   "(\\w+)\\s+(\\w+)\\s+(\\w+)\\s+(\\w+)\\s+(\\w+)"
+			    PCRE_STUDY_JIT_COMPILE)
+			  "The quick brown fox jumped")
+	       (#(string (0 . 26) (0 . 3) (4 . 9) (10 . 15) (16 . 19) (20 . 26)) #t)
+	       (_ #f)))
 
 (test-eqv "single utf code"
 	  1 (apply + (map (lambda (utf) (if (pcre-config utf) 1 0))
